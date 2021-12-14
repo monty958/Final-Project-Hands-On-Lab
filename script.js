00c6178fd89668ddc7ff4e3b5b6b46774144b1e5
Martin <86214313+monty958@users.noreply.github.com>
2021-12-14 14:13:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
