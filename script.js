18b32b87765a19cd810e1170ccb5ddae78894878
Martin <86214313+monty958@users.noreply.github.com>
2021-12-14 14:22:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
 
-var year = new Date().getFullYear()+parseInt(years);
-var interest = principal * years * rate /100;
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
 
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
 
+    document.getElementById("result").innerHTML = "Interest : If you deposit " + principal + ",<br/>" +
+            " at an interest rate of " + rate + ",<br/>" +
+            " You will receive an amount of " + interest + ",<br/> " +
+            " in the year " + yearInTheFuture + "<br/>";
+}
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    rateval = document.getElementById("rate_val").innerText;
 }
 
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
         
