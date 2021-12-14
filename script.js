2dffcd8d7ce93cdd7dbea6210c54e94dc52c7f57
Martin <86214313+monty958@users.noreply.github.com>
2021-12-14 14:15:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
 
+var year = new Date().getFullYear()+parseInt(years);
+var interest = principal * years * rate /100;
+
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
 
 function compute()
 {
